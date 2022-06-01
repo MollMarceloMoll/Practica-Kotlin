da411699749a32d20d0c847dc96ae34ddc4ac86b
Marcelo Moll <75146662+MollMarceloMoll@users.noreply.github.com>
2022-06-01 20:27:09 -0300

    Ejercicio Kotlin
    Tabla de multipicar con Kotlin

--- ejercicio03.kts
+++ ejercicio03.kts
@@ -1,13 +1,7 @@
 fun main() {
-    for(i in 1..50){
-        if (i % 3 == 0 && i % 5 == 0) {
-            println("$i FizzBuzz")
-        } else if (i % 5 == 0) {
-            println("$i Buzz")
-        } else if (i % 3 == 0) {
-            println("$i Fizz")
-        } else {
-            println(i)
-        }
+    val a: Int = 10
+    for (i in 1..10){
+        var result = a * i
+        println("$a * $i  = $result")
     }
 }
